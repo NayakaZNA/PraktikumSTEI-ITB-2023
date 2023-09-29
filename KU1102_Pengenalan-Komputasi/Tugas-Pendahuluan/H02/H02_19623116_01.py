701e7475a9f997fdf51b4708ae4fb31365820ad5
Nayaka <97321138+NayakaZNA@users.noreply.github.com>
2023-09-29 11:10:41 +0700

    Improve the code for H02.

---
+++ KU1102_Pengenalan-Komputasi/Tugas-Pendahuluan/H02/H02_19623116_01.py
@@ -0,0 +1,33 @@
+# NIM/Nama  : 19623116/Zulfaqqar Nayaka Athadiansyah
+# Tanggal   : 26 September 2023
+# Deskripsi : Program menentukan apakah suatu bilangan merupakan perpangkatan dari bilangan lain
+
+# KAMUS
+# n, nn, k : integer
+
+# ALGORITMA
+
+# Input
+n = int(input("Masukkan bilangan N: "))
+k = int(input("Masukkan nilai k: "))
+
+# Inisialisasi Awal
+nn = n
+
+# While loop (untuk k > 1)
+if k != 1:
+    # Jika n adalah perpangkatan dari k maka n = k^a untuk sembarang bilangan asli a. Jadi, n habis dibagi k.
+    # Akibatnya n bisa terus dibagi k (sebanyak a kali) hingga menjadi 1. Loop langsung dihentikan jika n tidak habis dibagi k lagi.
+    while (nn % k == 0):
+        nn /= k
+    if nn == 1:
+        print(f"{n} merupakan perpangkatan {k}.")
+    else: #nn != 1
+        print(f"{n} bukan merupakan perpangkatan {k}.")
+
+# Antisipasi corner case k = 1 (infinite loop)
+else: # k == 1
+    if n == 1:
+        nn = 1
+    else: # n != 1
+        print(f"{n} bukan merupakan perpangkatan {k}.")
