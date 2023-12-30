84bfc31bc2fde94a7b020a534756044476436d88
Nayaka <97321138+NayakaZNA@users.noreply.github.com>
2023-12-30 09:29:52 +0700

    Update H04_19623116_01.py

--- KU1102_PengenalanKomputasi/Tugas-Pendahuluan/H04/H04_19623116_01.py
+++ KU1102_PengenalanKomputasi/Tugas-Pendahuluan/H04/H04_19623116_01.py
@@ -1,6 +1,7 @@
 # NIM/Nama  : 19623116/Zulfaqqar Nayaka Athadiansyah
 # Tanggal   : 24 Oktober 2023
 # Deskripsi : Program menentukan jumlah maksimum submatriks 2x2 yang memuat elemen ganjil
+# (18 lines)
 
 # KAMUS
 # m, n            : integer             ; dimensi matriks
@@ -25,24 +26,18 @@ for i in range(m):
     row_matriks = input()
     ### Pecah string dalam `row_matriks` menjadi tiap elemen lalu masukkan ke `matriks`
     for j in range(n):
-        matriks[i][j] = int(row_matriks[2*j])
+        matriks[i][j] = int(row_matriks.split()[j])
 
 ## Proses
 ### For loop
-for i in range(m):
+for i in range(m-1):
     for j in range(1, n):
         ### Cari submatriks 2x2 yang punya elemen ganjil
-        if i < m - 1:
-            if matriks[i][j] % 2 == 1 or matriks[i][j-1] % 2 == 1:
-                ### Jumlahkan semua elemen submatriks tersebut, bandingkan dengan jumlah sebelumnya
-                if ganteng < matriks[i][j] + matriks[i][j-1] + matriks[i+1][j] + matriks[i+1][j-1]:
-                    ### Jika jumlahnya lebih besar, update nilai `ganteng` dengan jumlah tersebut
-                    ganteng = matriks[i][j] + matriks[i][j-1] + matriks[i+1][j] + matriks[i+1][j-1]
-        else: #i = m - 1
-            if matriks[i][j] % 2 == 1 or matriks[i][j-1] % 2 == 1:
-                ### idem
-                if ganteng < matriks[i][j] + matriks[i][j-1] + matriks[i-1][j] + matriks[i-1][j-1]:
-                    ganteng = matriks[i][j] + matriks[i][j-1] + matriks[i-1][j] + matriks[i-1][j-1]
+        if matriks[i][j] % 2 == 1 or matriks[i][j-1] % 2 == 1:
+            ### Jumlahkan semua elemen submatriks tersebut, bandingkan dengan jumlah sebelumnya
+            if ganteng < matriks[i][j] + matriks[i][j-1] + matriks[i+1][j] + matriks[i+1][j-1]:
+                ### Jika jumlahnya lebih besar, update nilai `ganteng` dengan jumlah tersebut
+                ganteng = matriks[i][j] + matriks[i][j-1] + matriks[i+1][j] + matriks[i+1][j-1]
 
 ## Output
 if ganteng == 0:
