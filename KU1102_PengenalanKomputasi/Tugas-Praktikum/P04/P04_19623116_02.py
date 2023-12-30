f4f78d32da1b183aa07e29ba8a7831480ad60f31
Nayaka <97321138+NayakaZNA@users.noreply.github.com>
2023-12-30 09:21:17 +0700

    Update P04_19623116_02.py

--- KU1102_PengenalanKomputasi/Tugas-Praktikum/P04/P04_19623116_02.py
+++ KU1102_PengenalanKomputasi/Tugas-Praktikum/P04/P04_19623116_02.py
@@ -13,61 +13,41 @@ m = int(input("Masukkan nilai m: "))
 n = int(input("Masukkan nilai n: "))
 
 ## Inisiasi matriks
-matriks1 = [[0 for i in range(n)] for i in range(m)]
-matriks2 = [[0 for i in range(n)] for i in range(m)]
+matriks1 = [[0 for j in range(n+2)] for i in range(m+2)]
+matriks2 = [[0 for j in range(n)] for i in range(m)]
 
 ## Input elemen matriks
 for i in range(m):
     for j in range(n):
-        matriks1[i][j] = int(input(f"Masukkan elemen matriks baris {i+1} kolom {j+1}: "))
+        matriks1[i+1][j+1] = int(input(f"Masukkan elemen matriks baris {i+1} kolom {j+1}: "))
 
 ## Proses
 for i in range(m):
     for j in range(n):
-        ### Baris paling atas
-        if i == 0:
-            ### Pojok kiri atas
-            if j == 0:
-                matriks2[i][j] = matriks1[i][j+1] + matriks1[i+1][j]
-            ### Pojok kanan atas
-            elif j == n - 1:
-                matriks2[i][j] = matriks1[i][j-1] + matriks1[i+1][j]
-            ### Elemen lainnya
-            else: # 0 < j < n - 1
-                matriks2[i][j] = matriks1[i][j-1] + matriks1[i][j+1] + matriks1[i+1][j]
-        
-        ### Baris paling bawah
-        elif i == m - 1:
-            ### Pojok kiri bawah
-            if j == 0:
-                matriks2[i][j] = matriks1[i][j+1] + matriks1[i-1][j]
-            ### Pojok kanan bawah
-            elif j == n - 1:
-                matriks2[i][j] = matriks1[i][j-1] + matriks1[i-1][j]
-            ### Elemen lainnya
-            else: # 0 < j < n - 1
-                matriks2[i][j] = matriks1[i][j-1] + matriks1[i][j+1] + matriks1[i-1][j]
-        
-        ### Kolom paling kiri (0 < i < m - 1, kasus i = 0 atau i = m - 1 sudah dicek di percabangan sebelumnya)
-        elif j == 0:
-            matriks2[i][j] = matriks1[i][j+1] + matriks1[i+1][j] + matriks1[i-1][j]
-        
-        ### Kolom paling kanan (0 < i < m - 1, kasus i=0 atau i=m-1 sudah dicek di percabangan sebelumnya)
-        elif j == n - 1:
-            matriks2[i][j] = matriks1[i][j-1] + matriks1[i+1][j] + matriks1[i-1][j]
-        
-        ### Elemen sisanya
-        else: # (0 < i < m - 1) and (0 < j < n - 1)
-            matriks2[i][j] = matriks1[i][j-1] + matriks1[i][j+1] + matriks1[i-1][j] + matriks1[i+1][j]
+        matriks2[i][j] = matriks1[i+1][j] + matriks1[i+1][j+2] + matriks1[i][j+1] + matriks1[i+2][j+1]
 
 ## Output
         if j == n - 1:
             print(matriks2[i][j])
         else: # 0 <= j < n - 1
             print(matriks2[i][j], end=" ")
-            
 
 """
+Penjelasan:
+Ambil contoh test case 1. Agar kode tidak terlalu panjang karena pembagian kasus yang banyak, 
+kita atur sehingga `matriks1` yang seharusnya berbentuk
+2 3 4
+2 2 3
+2 9 7
+
+menjadi
+
+0 0 0 0 0
+0 2 3 4 0
+0 2 2 3 0
+0 2 9 7 0
+0 0 0 0 0
+
 Semoga AC :D
 Semangat mengkoreksi, Kak Asprak!
 ~ Nayaka Ganteng
